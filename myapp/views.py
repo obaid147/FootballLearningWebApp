dc3cf50aaac111693542fcbefbcd4ac5fd198a8c
obaid147 <obaidfayazwani@gmail.com>
2020-12-01 12:57:05 +0530

    Register and login started

--- myapp/views.py
+++ myapp/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 def index(request):
     contents = {
         'title': 'Home',
-        'heading': 'Welcome To Soccer Learning'
+        'heading': 'Soccer Learning'
     }
     return render(request, 'index.html', contents)
 
